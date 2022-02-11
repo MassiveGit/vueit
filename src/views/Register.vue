1c704bc960b763e886ea3a0101d8e971a54f0006
Sam.Allen <Sam.Allen@datacom.co.nz>
2022-02-11 15:42:51 +1300

    Error responses from Signup api now logged

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -59,9 +59,11 @@ export default {
             console.log('Submitting User Signup request');
             if(!this.formData.username.length || this.formData.username.length < 5){
                 console.log("Username must be 5 or more Chars");
+                return;
             }
             if(!this.formData.password || this.formData.password.length < 8){
                 console.log("Password must be 8 or more Chars");
+                return;
             }
 
 
@@ -73,7 +75,11 @@ export default {
                 //Add response data to browser memory
                 //extract User name from data
             })
-
+          .catch((error) => {
+            console.log("User Signup request returned error status: " + error.response.status);
+            console.log(error.response.data);
+            console.log(error.response.headers);
+          })
         },
 
     },
